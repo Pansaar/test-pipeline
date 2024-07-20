6e124b87f73bbdf9f7272ac93c3b7ab4fec9b71c
pansaar kerdpol <pansaar@gmail.com>
2024-07-20 22:00:42 +0700

    Directory Config

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,9 +2,9 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull pansaar1553/simple-python-flask-app
+docker pull pansaar1553/test-pipeline-app
 
 # Run the Docker image as a container
-docker run -d -p 8000:8000 pansaar1553/simple-python-flask-app
+docker run -d -p 8000:8000 pansaar1553/test-pipeline-app
 
 #This is a test
