9ecb707e7547589b3880f16239a3b308a7cf4bad
pansaar kerdpol <pansaar@gmail.com>
2024-07-21 00:50:21 +0700

    Code change 2

--- code/app.py
+++ code/app.py
@@ -4,11 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # Print "Hello, world!" to the console
     print('Hello, world!')
-    return 'Hello, world!'
 
 if __name__ == '__main__':
-    # Print a startup message to the console
-    print('Starting Flask server...')
-    app.run(host='0.0.0.0', port=8000)  # Make sure it's accessible from the network
+    app.run()
+
+#This is a test
