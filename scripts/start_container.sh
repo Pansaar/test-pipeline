4ba3bffc5b394972fac5a701f9101cd6d87978c9
pansaar kerdpol <pansaar@gmail.com>
2024-07-21 11:31:55 +0700

    Start Container Modification

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,3 +1,6 @@
 set -e
 docker pull pansaar1553/test-pipeline-app
+if [ "$(docker ps -a -q -f name=testpipeline)" ]; then
+    docker rm testpipeline
+fi
 docker run -d -p 8000:8000 --name testpipeline pansaar1553/test-pipeline-app
