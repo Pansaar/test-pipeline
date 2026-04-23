353268f3e2aaad610a086edfa14de1ac892c66e8
Pansaar <your@email.com>
2026-04-23 10:26:00 +0700

    Edit function app powershell

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 data "azurerm_resource_group" "pansaar-rg" {
-  name     = "pansaar-rg"
+  name = "pansaar-rg"
 }
 
 resource "azurerm_storage_account" "functionsa" {
@@ -15,25 +15,29 @@ resource "azurerm_app_service_plan" "function_plan" {
   location            = data.azurerm_resource_group.pansaar-rg.location
   resource_group_name = data.azurerm_resource_group.pansaar-rg.name
   kind                = "FunctionApp"
+
   sku {
     tier = "Dynamic"
     size = "Y1"
   }
 }
 
-resource "azurerm_function_app" "function_app" {
+resource "azurerm_windows_function_app" "function_app" {
   name                       = "pansaar-func-app"
   location                   = data.azurerm_resource_group.pansaar-rg.location
   resource_group_name        = data.azurerm_resource_group.pansaar-rg.name
-  app_service_plan_id        = azurerm_app_service_plan.function_plan.id
+  service_plan_id            = azurerm_app_service_plan.function_plan.id
   storage_account_name       = azurerm_storage_account.functionsa.name
   storage_account_access_key = azurerm_storage_account.functionsa.primary_access_key
-  version                    = "~4"
-  os_type                    = "windows"
+
+  site_config {
+    application_stack {
+      powershell_core_version = "7.2"
+    }
+  }
 
   app_settings = {
-    FUNCTIONS_WORKER_RUNTIME        = "powershell"
-    WEBSITE_RUN_FROM_PACKAGE        = "1"
-    FUNCTIONS_WORKER_RUNTIME_VERSION = "7.2"
+    FUNCTIONS_WORKER_RUNTIME = "powershell"
+    WEBSITE_RUN_FROM_PACKAGE = "1"
   }
 }
